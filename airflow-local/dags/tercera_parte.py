2f363b523ceae420b58140d7f7d865287b4b719d
Jesús Gómez Álvarez <jegomezalvarez@gmail.com>
2022-05-29 17:06:46 +0200

    Tercera parte 3

--- airflow-local/dags/tercera_parte.py
+++ airflow-local/dags/tercera_parte.py
@@ -23,6 +23,17 @@ with DAG(
 ) as dag:
     start = DummyOperator(task_id='start', dag=dag)
     end = DummyOperator(task_id='end', dag=dag)
+    task_1 = DummyOperator(task_id='task_1', dag=dag)
+    task_2 = DummyOperator(task_id='task_2', dag=dag)
+    task_3 = DummyOperator(task_id='task_3', dag=dag)
+    task_4 = DummyOperator(task_id='task_4', dag=dag)
+    task_5 = DummyOperator(task_id='task_5', dag=dag)
+    task_6 = DummyOperator(task_id='task_6', dag=dag)
 
 # 2
-start >> end
+# start >> end
+
+# 3
+start >> [task_1,task_3,task_5] >> task_2 >> end
+start >> [task_1,task_3,task_5] >> task_4 >> end
+start >> [task_1,task_3,task_5] >> task_6 >> end
